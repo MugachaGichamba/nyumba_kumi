1ed585bb0058f90a0d08680ae33147654135e1f4
willy_kid <nillyw31@gmail.com>
2019-06-03 17:42:31 +0300

    created user login and logout urls

--- nyumba_kumi/urls.py
+++ nyumba_kumi/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name="register"),
     path('', user_views.home, name="home"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
