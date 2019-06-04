3812586e915cca073b6016cbe2fa7e5e182f6d29
willy_kid <nillyw31@gmail.com>
2019-06-04 13:06:53 +0300

    created hood model

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Hood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
+
+
+# class Business(models.Model):
+#     email = models.EmailField(blank=False, null=False)
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     business = models.ForeignKey(Hood, on_delete=models.CASCADE)
+#
+#     def get_absolute_url(self):
+#         return reverse('home')
