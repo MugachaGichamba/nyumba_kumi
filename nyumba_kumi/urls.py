de38367fd619e30db58f2d3709c33bf721bf7b76
willy_kid <nillyw31@gmail.com>
2019-06-04 12:46:50 +0300

    defined user profile routes

--- nyumba_kumi/urls.py
+++ nyumba_kumi/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name="register"),
     path('', user_views.home, name="home"),
+    path('profile/', user_views.profile, name="profile"),
+    path('update_profile/', user_views.update_profile, name="update"),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 ]
