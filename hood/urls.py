f11b5112792657246a8d4ecde4d43c3c86dbffd0
willy_kid <nillyw31@gmail.com>
2019-06-04 13:30:58 +0300

    added business list view url to hood urls

---
+++ hood/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path('new_project/', views.ProjectCreateView.as_view(), name='project-create'),
+    path('', views.ProjectListView.as_view(), name="home"),
+    # # path('rate_post/(?P<class>\d+)/$', CreateSessionsView.as_view(), name='create_sessions')
+    # path('rate_project/<int:project_id>/', views.RateProjectCreateView.as_view(), name='rate_project'),
+    # path('project_detail/<int:pk>/', views.ProjectDetailView.as_view(), name='project-detail'),
+    # path('view_ratings/<int:project_id>/', views.ViewRatings.as_view(), name='view_ratings'),
+
+]
--- hood/views.py
+++ hood/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Business
 from django.views.generic import CreateView, ListView, DetailView
 
+
 class ProjectListView(ListView):
     model = Business
 
