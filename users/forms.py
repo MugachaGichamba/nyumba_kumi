21ac664ba641e4eea38a10c98cc12fd21343b7ac
willy_kid <nillyw31@gmail.com>
2019-06-04 12:49:28 +0300

    created userupdateform

--- users/forms.py
+++ users/forms.py
@@ -11,13 +11,13 @@ class UserRegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-#
-# class UserUpdateForm(forms.ModelForm):
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email']
-#
-#
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
 # class ProfileUpdateForm(forms.ModelForm):
 #     class Meta:
 #         model = Profile
--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from .forms import UserRegisterForm
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
+
 def register(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
@@ -23,3 +24,30 @@ def home(request):
 
     return render(request, "users/home.html")
 
+#
+# @login_required
+# def profile(request):
+#     return render(request, 'users/profile.html')
+#
+#
+# @login_required
+# def update_profile(request):
+#     if request.method == 'POST':
+#         u_form = UserUpdateForm(request.POST, instance=request.user)
+#         p_form = ProfileUpdateForm(request.POST,
+#                                    request.FILES,
+#                                    instance=request.user.profile)
+#
+#         if u_form.is_valid() and p_form.is_valid():
+#             u_form.save()
+#             p_form.save()
+#             messages.success(request, "Your account has been updated")
+#             return redirect('profile')
+#     else:
+#         u_form = UserUpdateForm(instance=request.user)
+#         p_form = ProfileUpdateForm(instance=request.user.profile)
+#     context = {
+#         'u_form': u_form,
+#         'p_form': p_form
+#     }
+#     return render(request, 'users/update_profile.html', context)
